594fd00bc58e1632decae1be83cdb784eb4455b8
Ben Shepherd <bjashepherd@gmail.com>
2021-07-28 10:37:46 +0100

    send notification

--- check-leave-dates.py
+++ check-leave-dates.py
@@ -1,6 +1,8 @@
 import pandas
 from oracle import go_to_oracle_page
 from outlook import get_appointments_in_range
+from pushbullet import Pushbullet  # to show notifications
+from pushbullet_api_key import api_key  # local file, keep secret!
 
 web = go_to_oracle_page(('RCUK Self-Service Employee', 'Attendance Management'))
 cells = web.driver.find_elements_by_class_name('x1w')
@@ -14,5 +16,12 @@ outlook_off_dates = set(sum([pandas.bdate_range(ev.Start.date(), ev.End.date(), 
                              for ev in get_appointments_in_range(min(oracle_off_dates), 0)
                              if ev.AllDayEvent and ev.BusyStatus == 3 and ev.Subject == 'Annual Leave'], []))
 
-print('Not in Outlook', oracle_off_dates - outlook_off_dates)
-print('Not in Oracle', outlook_off_dates - oracle_off_dates)
+not_in_outlook = oracle_off_dates - outlook_off_dates
+print('Not in Outlook', not_in_outlook)
+not_in_oracle = outlook_off_dates - oracle_off_dates
+print('Not in Oracle', not_in_oracle)
+dmy = "%#d/%#m/%Y"
+toast = ('Missing from Outlook: ' + ', '.join(date.strftime(dmy) for date in not_in_outlook)) if not_in_outlook else ''
+toast += ('\nMissing from Oracle: ' + ', '.join(date.strftime(dmy) for date in not_in_oracle)) if not_in_oracle else ''
+if toast:
+    Pushbullet(api_key).push_note('📅 Check leave dates', toast)
