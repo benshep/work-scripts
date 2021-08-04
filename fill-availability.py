af6f01afb72e8e8882abb040b850a250bf125a8a
Ben Shepherd <bjashepherd@gmail.com>
2021-08-04 14:02:06 +0100

    tidy up

--- fill-availability.py
+++ fill-availability.py
@@ -10,32 +10,33 @@ d_mon = "%#d %b"  # e.g. 4 Aug
 available_dates = set(pandas.bdate_range('today', periods=5*weeks).tolist())
 print('Fixed events:')
 for event in get_appointments_in_range(0, 7 * weeks):
-    # print(event.Subject, event.IsRecurring, event.GetRecurrencePattern().RecurrenceType)
     recurrence = event.GetRecurrencePattern()
     weekly = event.IsRecurring and recurrence.RecurrenceType == 1 and recurrence.Interval == 1
     busy = event.BusyStatus == 2
-    oof = event.BusyStatus == 3
+    away = event.BusyStatus == 3
     people = len(event.RequiredAttendees.split('; '))
     hours = event.Duration / 60
-    fixed = (oof or (busy and not weekly)) and (people < 2 or people > 4 or hours >= 2)
-    if fixed:
-        print(event.Subject, event.Start.strftime(d_mon+' %H:%M'), 'busy' if busy else '',
-              'oof' if oof else '', 'weekly' if weekly else '', f"{people=} {hours=:.1f}")
-        # remove dates in range (Start, End) from available_dates
+    days = hours / 24
+    if (away or (busy and not weekly)) and (people < 2 or people > 5 or hours >= 2):
+        all_day = event.AllDayEvent
+        print(event.Subject, event.Start.strftime(d_mon + (' %H:%M' if not all_day else '')),
+              ', '.join(key for key, value in locals().items() if value is True),
+              f"{people=}", f"{days=}" if all_day else f"{hours=:.1f}")
         available_dates -= set(pandas.bdate_range(event.Start.date(), event.End.date()).tolist())
 
 print('\nLooking for changes')
 spreadsheet_xlsx = os.path.join(os.environ['UserProfile'], 'OneDrive - Science and Technology Facilities Council',
                                 'CLARA_Shifts_rota', 'Ben.xlsx')
 mod_time = pandas.to_datetime(os.path.getmtime(spreadsheet_xlsx), unit='s')
-old_end_date = mod_time + pandas.to_timedelta(7 * weeks, 'day')
+old_end_date = mod_time + pandas.to_timedelta(weeks, 'W')
 column_name = 'available'
 old_free_dates = pandas.read_excel(spreadsheet_xlsx)[column_name]  # returns a Series
 new_data = pandas.DataFrame(sorted(list(available_dates)), columns=[column_name])
+today = pandas.to_datetime('today').floor('d')
 with pandas.ExcelWriter(spreadsheet_xlsx, mode='a', if_sheet_exists='replace') as writer:
     new_data.to_excel(writer, sheet_name='Ben')
-    
-today = pandas.to_datetime('today').round('d')
+    pandas.DataFrame([today]).to_excel(writer, sheet_name='Updated')
+
 print(f"Old list has {len(old_free_dates)} entries, file modified {mod_time.strftime(d_mon)}")
 old_free_dates = old_free_dates[old_free_dates >= today]
 print(f'Ignoring dates in old list before today, {today.strftime(d_mon)} - reduced to {len(old_free_dates)} entries')
@@ -49,9 +50,9 @@ changed_to_yes = new_free_dates - old_free_dates
 changed_to_no = old_free_dates - new_free_dates
 toast = ''
 if changed_to_yes:
-    toast += 'Changed to Yes: ' + ', '.join(date.strftime(d_mon) for date in changed_to_yes) + '\n'
+    toast += '👍 ' + ', '.join(date.strftime(d_mon) for date in changed_to_yes) + '\n'
 if changed_to_no:
-    toast += 'Changed to No: ' + ', '.join(date.strftime(d_mon) for date in changed_to_no)
+    toast += '👎 ' + ', '.join(date.strftime(d_mon) for date in changed_to_no)
 
 if toast:
     print(toast)
