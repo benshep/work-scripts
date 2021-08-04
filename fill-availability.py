26bfb3410b8ca21b3fb86b9562fbb9827e84298f
Ben Shepherd <bjashepherd@gmail.com>
2021-08-04 10:22:26 +0100

    toast with changes

--- fill-availability.py
+++ fill-availability.py
@@ -1,26 +1,58 @@
 import os
 import pandas
-from datetime import datetime, timedelta
 from outlook import get_appointments_in_range
+from pushbullet import Pushbullet  # to show notifications
+from pushbullet_api_key import api_key  # local file, keep secret!
 
-today = datetime.today()
-available_dates = set(pandas.bdate_range(today, today + timedelta(days=60)).tolist())
-for appt in get_appointments_in_range(0, 60):
-    # print(appt.Subject, appt.IsRecurring, appt.GetRecurrencePattern().RecurrenceType)
-    recurrence = appt.GetRecurrencePattern()
-    weekly = appt.IsRecurring and recurrence.RecurrenceType == 1 and recurrence.Interval == 1
-    busy = appt.BusyStatus == 2
-    oof = appt.BusyStatus == 3
-    people = len(appt.RequiredAttendees.split('; '))
-    hours = appt.Duration / 60
+weeks = 8
+d_mon = "%#d %b"  # e.g. 4 Aug
+
+available_dates = set(pandas.bdate_range('today', periods=5*weeks).tolist())
+print('Fixed events:')
+for event in get_appointments_in_range(0, 7 * weeks):
+    # print(event.Subject, event.IsRecurring, event.GetRecurrencePattern().RecurrenceType)
+    recurrence = event.GetRecurrencePattern()
+    weekly = event.IsRecurring and recurrence.RecurrenceType == 1 and recurrence.Interval == 1
+    busy = event.BusyStatus == 2
+    oof = event.BusyStatus == 3
+    people = len(event.RequiredAttendees.split('; '))
+    hours = event.Duration / 60
     fixed = (oof or (busy and not weekly)) and (people < 2 or people > 4 or hours >= 2)
     if fixed:
-        print(appt.Subject, appt.Start, busy, oof, weekly, people, hours, fixed)
+        print(event.Subject, event.Start.strftime(d_mon+' %H:%M'), 'busy' if busy else '',
+              'oof' if oof else '', 'weekly' if weekly else '', f"{people=} {hours=:.1f}")
         # remove dates in range (Start, End) from available_dates
-        available_dates -= set(pandas.bdate_range(appt.Start.date(), appt.End.date()).tolist())
+        available_dates -= set(pandas.bdate_range(event.Start.date(), event.End.date()).tolist())
 
-df = pandas.DataFrame(available_dates)
+print('\nLooking for changes')
 spreadsheet_xlsx = os.path.join(os.environ['UserProfile'], 'OneDrive - Science and Technology Facilities Council',
                                 'CLARA_Shifts_rota', 'Ben.xlsx')
+mod_time = pandas.to_datetime(os.path.getmtime(spreadsheet_xlsx), unit='s')
+old_end_date = mod_time + pandas.to_timedelta(7 * weeks, 'day')
+column_name = 'available'
+old_free_dates = pandas.read_excel(spreadsheet_xlsx)[column_name]  # returns a Series
+new_data = pandas.DataFrame(sorted(list(available_dates)), columns=[column_name])
 with pandas.ExcelWriter(spreadsheet_xlsx, mode='a', if_sheet_exists='replace') as writer:
-    df.to_excel(writer, sheet_name='Ben')
+    new_data.to_excel(writer, sheet_name='Ben')
+    
+today = pandas.to_datetime('today').round('d')
+print(f"Old list has {len(old_free_dates)} entries, file modified {mod_time.strftime(d_mon)}")
+old_free_dates = old_free_dates[old_free_dates >= today]
+print(f'Ignoring dates in old list before today, {today.strftime(d_mon)} - reduced to {len(old_free_dates)} entries')
+new_free_dates = new_data[column_name]
+print(f"New list has {len(new_free_dates)} entries")
+new_free_dates = new_free_dates[new_free_dates < old_end_date]
+print(f'Ignoring dates in new list after {old_end_date.strftime(d_mon)} - reduced to {len(new_free_dates)} entries')
+old_free_dates = set(old_free_dates)
+new_free_dates = set(new_free_dates)
+changed_to_yes = new_free_dates - old_free_dates
+changed_to_no = old_free_dates - new_free_dates
+toast = ''
+if changed_to_yes:
+    toast += 'Changed to Yes: ' + ', '.join(date.strftime(d_mon) for date in changed_to_yes) + '\n'
+if changed_to_no:
+    toast += 'Changed to No: ' + ', '.join(date.strftime(d_mon) for date in changed_to_no)
+
+if toast:
+    print(toast)
+    Pushbullet(api_key).push_note('📅 Availability', toast)
