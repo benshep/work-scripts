317bc9fa399ab4ea318d31f3d18b8bb4e5b1d67e
Ben Shepherd <bjashepherd@gmail.com>
2021-09-03 15:54:15 +0100

    look up away days from previous month too

--- enter_otl_timecard.py
+++ enter_otl_timecard.py
@@ -16,31 +16,35 @@ ftes = ftes.iloc[:-1]  # remove total column
 ftes = ftes.dropna()  # get rid of projects with zero hours
 hours = ftes * 7.4  # hours per day for each project
 
-# find out of office dates in the next 3 months
+# find out of office dates in the next 3 months (and previous 1)
 days_away = []
-for appointment in get_appointments_in_range(0, 90):
+for appointment in get_appointments_in_range(-30, 90):
     if appointment.AllDayEvent and appointment.BusyStatus == 3:  # out of office
         days = appointment.End - appointment.Start
         days_away += [appointment.Start + timedelta(days=i) for i in range(days.days)]
 days_away = [date.replace(tzinfo=None) for date in days_away]  # ignore time zone info
+print('Out of office days', days_away)
 
-web = go_to_oracle_page(('STFC OTL Timecards', 'Time'))
+web = go_to_oracle_page(('STFC OTL Timecards', 'Time'), show_window=True)
 
 toast = ''
 while True:
+    print('Going to timesheet page')
     web.click('Create Timecard')
     web.refresh()
-    web.type('Angal-Kalinin, Doctor Deepa (Deepa)', id='A150N1display')  # approver
     select = web.find_elements(id='N66')[0]
     options = select.find_elements_by_tag_name('option')
     for option in options:
         card_label = option.text
         if not card_label.endswith('~'):  # not entered yet
+            print('Creating timecard for', card_label)
             option.click()
+            web.type('Angal-Kalinin, Doctor Deepa (Deepa)', id='A150N1display')  # approver
             wb_date = datetime.strptime(card_label.split(' - ')[0], '%B %d, %Y')  # e.g. August 16, 2021
             if pandas.Timestamp(wb_date + timedelta(days=5)).to_period('Q-MAR').qyear != fy:
                 raise NotImplementedError('Going into new financial year not supported yet')
             on_holiday = [wb_date + timedelta(days=day) in days_away for day in range(5)]  # list of True/False for on holiday that day
+            print(f'{on_holiday=}')
 
             # enter hours
             if not all(on_holiday):
