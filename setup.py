6df24c795df91fcd8d0dc637aabd32c75513186b
Ben Shepherd <bjashepherd@gmail.com>
2021-10-15 14:38:56 +0100

    setup.py, requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Scripts',
+    version='',
+    packages=['pandas', 'pushbullet.py', 'pywin32', 'webbot'],
+    url='',
+    license='',
+    author='benshep',
+    author_email='',
+    description=''
+)
