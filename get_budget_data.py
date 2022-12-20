f1f5e148d41143cae26a494af78c51fe3391e42b
Ben Shepherd <bjashepherd@gmail.com>
2022-12-20 09:09:32 +0000

    move spreadsheet to new folder

--- get_budget_data.py
+++ get_budget_data.py
@@ -85,7 +85,7 @@ def get_budget_data(project_names='all', show_window=True):
     user_profile = os.environ['UserProfile']
     today = datetime.now()
     fy = today.year - (today.month < 4)  # last calendar year if before April
-    excel_filename = os.path.join(user_profile, 'Documents', 'Budgets', f'Budget summaries {fy}.xlsx')
+    excel_filename = os.path.join(user_profile, 'Documents', 'Budget', f'Budget summaries {fy}.xlsx')
     # The Index sheet here must have columns Name, Project and Task at least.
     # Budget data will be placed into sheets named after the Name column, overwriting anything already in there.
     csv_filename = os.path.join(user_profile, 'Downloads', 'Detailed Cost by project.csv')
