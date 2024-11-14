70d8ae0e64e60b640216719c6f44cc49e01d206b
Ben Shepherd <bjashepherd@gmail.com>
2024-11-14 09:36:57 +0000

    multi-install statements

--- package_updates.py
+++ package_updates.py
@@ -36,13 +36,16 @@ def find_new_python_packages():
     installed_packages = list_packages()
     available_packages = get_rss()
     toast = ''
+    conda_new = []
+    pip_new = []
     for name, (version, build_channel) in installed_packages.items():
         new_version = available_packages.get(name, '')
         if new_version > version:
             if 'numpy' in name and new_version == '2.0.0':
                 continue  # numpy upgrades aren't working right now
-            toast += (f'pip install {name} -U' if 'pypi' in build_channel else f'conda upgrade {name}') + \
-                     f' # {version} --> {new_version}' + '\n'
+            (pip_new if 'pypi' in build_channel else conda_new).append(name)
+    toast = (f'conda upgrade {" ".join(conda_new)}\n' if conda_new else '') + \
+            (f'pip install {" ".join(pip_new)}' if pip_new else '')
     return toast
 
 
