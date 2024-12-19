7119b9213c5161c05731a1dde6fdd21baee37530
Ben Shepherd <bjashepherd@gmail.com>
2024-12-19 14:20:15 +0000

    mars_group.py

---
+++ mars_group.py
@@ -0,0 +1,2 @@
+members = ['Ben Shepherd', 'Neil Thompson', 'Kiril Marinov', 'Alan Mak', 'Dave Dunning', 'Amy Pollard', 'Matt King',
+           'Nasiq Ziyan', 'Alex Bainbridge', 'Alex Hinton', 'Tom Smith']
