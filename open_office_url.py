0d93a4b13abd45d6649f52501119cc2b029e340f
Ben Shepherd <bjashepherd@gmail.com>
2025-01-17 09:14:47 +0000

    file type in URL or link text

--- open_office_url.py
+++ open_office_url.py
@@ -10,19 +10,21 @@ def open_office_url(extra_info):
     The extra_info argument is the base64 encoded [EXTRA] parameter from the External Application Launcher addon.
     It decodes to a JSON object with parameters: (for context menu clicks)
     menuItemId, parentMenuItemId, viewType, frameId, editable, linkText, linkUrl, pageUrl, modifiers, button"""
-    info = json.loads(b64decode(extra_info))
-    link_text = info['linkText']
-    if '.doc' in link_text:
+    info_decoded = b64decode(extra_info).decode('utf-8')
+    info_json = json.loads(info_decoded)
+    # open(r'C:\Users\bjs54\Downloads\output.txt', 'w').write(str(info))
+    # check for file extension: could be in text or URL, so just check JSON string
+    if '.doc' in info_decoded:
         app = 'winword.exe'
-    elif '.ppt' in link_text:
+    elif '.ppt' in info_decoded:
         app = 'powerpnt.exe'
-    elif '.xls' in link_text:
+    elif '.xls' in info_decoded:
         app = 'excel.exe'
     else:
         return
     office_path = r'C:\Program Files\Microsoft Office\root\Office16'
-    url = info['linkUrl']
     # try leaving in the query portion of the URL (after ?) - if it doesn't work, need to be discerning
+    url = info_json['linkUrl']
     # url = url.split('?')[0]
     subprocess.call([os.path.join(office_path, app), url])
 
