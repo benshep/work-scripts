0787463e8f7d95e49f49336e9590c9770552d585
Ben Shepherd <bjashepherd@gmail.com>
2025-11-05 09:05:46 +0000

    working version

--- energy_data.py
+++ energy_data.py
@@ -1,60 +1,128 @@
-from datetime import datetime
+import asyncio
+import os
+import sys
+import pandas
+from openpyxl import load_workbook
+from openpyxl.styles import Font, Alignment
 
 from rayleigh_connect import EasyAPI
 from rayleigh_api_key import api_key
+from folders import docs_folder, misc_folder
+from work_tools import read_excel
+
+sys.path.append(os.path.join(misc_folder, 'Scripts'))
+from get_energy_usage import get_co2_data
 
 rayleigh = EasyAPI(api_key)
 
 
-def find_sensors(names: list[str]) -> dict[str, tuple[str, str]]:
+async def find_sensors(names: list[str]) -> dict[str, tuple[str, str]]:
     """Returns the gateway and sensor IDs for the given sensor names."""
     return_dict = {}
-    for gateway in rayleigh.fetch_gateways():
-        for sensor in rayleigh.fetch_sensors(gateway['id']):
+    for gateway in await rayleigh.fetch_gateways():
+        for sensor in await rayleigh.fetch_sensors(gateway['id']):
             if sensor['name'] in names:
                 return_dict[sensor['name']] = (gateway['id'], sensor['id'])
     return return_dict
 
 
-def list_sensors():
+async def list_sensors():
     """Lists sensor names defined for each gateway."""
-    for gateway in rayleigh.fetch_gateways():
-        for sensor in rayleigh.fetch_sensors(gateway['id']):
+    for gateway in await rayleigh.fetch_gateways():
+        for sensor in await rayleigh.fetch_sensors(gateway['id']):
             if sensor['name'] not in (None, 'Frequency'):  # lots of sensors called Frequency
-                print(sensor['name'])
+                print(gateway['id'], sensor['id'], sensor['name'], sep='\t')
 
 
-def archive_data():
-    meters = [
-        ('Q3025301880080025', 'e5'),
-        ('Q3025301880080025', 'e6')
-    ]
-    gateways = rayleigh.fetch_gateways()
-    # print(*gateways, sep='\n')
-    for gateway in gateways:
-        print(gateway['id'], gateway['name'])
-        sensors = rayleigh.fetch_sensors(gateway['id'])
-        for sensor in sensors:
-            if (gateway['id'], sensor['id']) in meters:
-                data = rayleigh.fetch_data(
-                    gateway['id'],
-                    sensor['id'] + '.kwh',  # want the energy data
-                    datetime(2025, 10, 23))
-                print(data)
+def update_energy_data():
+    """Sync wrapper for async archive_data function."""
+    asyncio.run(archive_data())
 
 
-# Data returned has arbitrary timestamps. Need to use something like numpy.interp1d
-# to peg it to half-hourly intervals. Then drop into an XLSX file with structure:
-# datetime          gateway_id1     gateway_id2
-#                   sensor_id1      sensor_id2
-#                   sensor_name1    sensor_name2
-# 2025-10-24 00:00  123.456         456.789
-# 2025-10-24 00:30  123.556         456.889
-# ...
+async def archive_data():
+    """Fetch new energy and carbon data and drop it into the spreadsheet."""
+    excel_filename = os.path.join(docs_folder, 'CLARA', 'CLARA electricity usage.xlsx')
+    meters_sheet = 'Meters'
+    # Get the list of sensors from the top 3 rows of this Excel file
+    workbook = read_excel(excel_filename, header=[0, 1, 2], sheet_name=None, index_col=0)
+    meter_data = workbook[meters_sheet]
+    # Use the first column of the file to get a start date for our data
+    last_timestamp = max(meter_data.index)
+    start = last_timestamp - pandas.to_timedelta(1, unit='hours')  # go back a bit, to avoid gaps
+    end = last_timestamp + pandas.to_timedelta(14, 'days')
+    # print(meter_data.columns[:3])
+    frames = await asyncio.gather(
+        *[rayleigh.fetch_data(
+            gateway,
+            sensor + '.kwh',  # want the energy data
+            start.to_pydatetime(),
+            end.to_pydatetime(),
+        ) for gateway, sensor, sensor_name in meter_data.columns])
+    # print(frames)
+    # Resample so that data points are on half-hour intervals
+    frames = [resample(frame, key) for frame, key in zip(frames, meter_data.columns)]
+    # What is the last data point common to all datasets? Trying to avoid 'orphans'
+    first_new_time = last_timestamp + pandas.to_timedelta(30, unit='minutes')
+    last_new_time = min(max(frame.index) for frame in frames if len(frame) > 0)
+    # Concatenate all the data together horizontally...
+    new_data = pandas.concat(frames, axis=1)
+    # Delete all rows with data after this last common point, and remove any overlap
+    new_data = new_data.loc[first_new_time:last_new_time]
+    print('New data from', first_new_time, 'to', last_new_time)
+    # and add it to the pre-existing Excel data
+    meter_data = pandas.concat([meter_data, new_data])
+    # print(meter_data)
+
+    # Get the carbon intensity data
+    intensity_sheet = 'Intensity'
+    intensity_data = workbook[intensity_sheet]
+    first_new_int_time = max(intensity_data.index) + pandas.to_timedelta(30, 'min')
+    new_intensity_data = get_co2_data(first_new_int_time, 'WA4', do_pivot=False, end=last_new_time)
+    if not new_intensity_data.empty:
+        new_intensity_data.index = new_intensity_data.index.tz_localize(None)  # make timezone-unaware for Excel
+        # print(new_intensity_data)
+        new_intensity_data.columns = intensity_data.columns
+        intensity_data = pandas.concat([intensity_data, new_intensity_data])
+
+    # Write the old and new data back into the Excel workbook
+    with pandas.ExcelWriter(excel_filename, mode='a', if_sheet_exists='replace', engine='openpyxl') as writer:
+        meter_data.to_excel(writer, sheet_name=meters_sheet, merge_cells=False)
+        intensity_data.to_excel(writer, sheet_name=intensity_sheet, merge_cells=True)
+
+    # Carry out some formatting
+    workbook = load_workbook(excel_filename)
+    bold_format = Font(bold=True)
+    word_wrap = Alignment(wrap_text=True)
+    for sheet_name in (meters_sheet, intensity_sheet):
+        worksheet = workbook[sheet_name]
+        worksheet.column_dimensions['A'].width = 20  # otherwise can't read dates
+        for col in range(2, worksheet.max_column + 1):
+            column_name = worksheet.cell(4, col).column_letter  # use row 4 so it's not merged
+            worksheet.column_dimensions[column_name].width = 18
+        for row in worksheet.iter_rows(1, 3):
+            for cell in row:
+                cell.font = bold_format
+                if cell.row == 3:
+                    cell.alignment = word_wrap
+    workbook.save(excel_filename)
+
+
+def resample(data: list[list], key: tuple[str, str, str]):
+    """Convert data in 2d list format to a DataFrame and resample to half-hourly intervals."""
+    df = pandas.DataFrame(data, columns=['timestamp', 'reading'])
+    df['timestamp'] = pandas.to_datetime(df['timestamp'])
+    df.set_index('timestamp', inplace=True)
+    df.index = df.index.tz_localize(None)  # make timezone-unaware for Excel
+    df = df.resample(pandas.Timedelta('30min')).ffill()
+    df = df.dropna()
+    df.columns = pandas.MultiIndex.from_tuples([key])
+    # print(df.columns)
+    # print(df)
+    return df
 
 
 if __name__ == '__main__':
     # print(find_sensors(['PD2 - SW4E - PD934 CLARA Rack Room 2 - Magnet Rack 1',
     #                     'PD2 - SW4D - PD935 CLARA Rack Room 2 - Magnet Rack 2']))
     # list_sensors()
-    archive_data()
+    update_energy_data()
