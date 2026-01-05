5624886a0dd2d902ad22723069792afae08259bf
Ben Shepherd <bjashepherd@gmail.com>
2026-01-05 12:17:12 +0000

    write links to file instead of navigating directly

--- group.py
+++ group.py
@@ -5,33 +5,40 @@ from urllib.parse import urlencode
 import oracle
 import staff
 from mars_group import members
+from work_folders import downloads_folder
 
 
 def run_otl_calculator() -> str | None:
     """Iterate through staff, listing the hours to upload for new OTL cards required."""
     # staff.verbose = True
     cards_to_book = 0
-    for member in members:
-        # if member.known_as in ('Nasiq',):
-        if True:
-            print('\n', member.name)
-            member.update_off_days(force_reload=False)
-            start = date.today()
-            start -= timedelta(days=start.weekday())  # Monday of current week
-            start -= timedelta(days=7*4)  # check last few weeks
-            # while start + timedelta(days=3) <= date.today():  # wait until Thu to do current week
-            for _ in range(7):
-                hours_booked = member.hours_for_week(start)
-                hours_needed = sum(member.hours_needed(start + timedelta(days=day)) for day in range(5))
-                print(f'{start.strftime("%d/%m/%Y")}: {hours_booked=:.2f}, {hours_needed=:.2f}')
-                if hours_needed - hours_booked > 0.01:
-                    print(*member.bulk_upload_lines(start, manual_mode=True), sep='\n')
-                    cards_to_book += 1
-                    params = {'calledFromAddTimeCard': 'true', 'pAsofdate': start.strftime('%Y-%m-%d')}
-                    if member.known_as != 'Ben':
-                        params |= {'pPersonId': member.person_id, 'userContext': 'LINE_MANAGER'}
-                    # os.startfile(oracle.apps[('home',)] + 'time/timecards/landing-page?' + urlencode(params))
-                start += timedelta(days=7)
+    with open(os.path.join(downloads_folder, 'otl_upload_links.html'), 'w') as links_file:
+        for member in members:
+            # if member.known_as in ('Nasiq',):
+            if True:
+                links_file.write(f'<h2>{member.known_as}</h2>\n')
+                print('\n', member.name)
+                member.update_off_days(force_reload=False)
+                start = date.today()
+                start -= timedelta(days=start.weekday())  # Monday of current week
+                start -= timedelta(days=7*4)  # check last few weeks
+                while start + timedelta(days=3) <= date.today():  # wait until Thu to do current week
+                # for _ in range(7):
+                    hours_booked = member.hours_for_week(start)
+                    hours_needed = sum(member.hours_needed(start + timedelta(days=day)) for day in range(5))
+                    print(f'{start.strftime("%d/%m/%Y")}: {hours_booked=:.2f}, {hours_needed=:.2f}')
+                    if hours_needed - hours_booked > 0.01:
+                        cards_to_book += 1
+                        params = {'calledFromAddTimeCard': 'true', 'pAsofdate': start.strftime('%Y-%m-%d')}
+                        if member.known_as != 'Ben':
+                            params |= {'pPersonId': member.person_id, 'userContext': 'LINE_MANAGER'}
+                        url = oracle.apps[('home',)] + 'time/timecards/landing-page?' + urlencode(params)
+                        links_file.write(f'<p><a href="{url}">{start.strftime("%d/%m/%Y")}</a></p>\n')
+                        lines = list(member.bulk_upload_lines(start, manual_mode=True))
+                        print(*lines, sep='\n')
+                        for line in lines:
+                            links_file.write(f'<p>{line}</p>\n')
+                    start += timedelta(days=7)
     if cards_to_book:
         return f'{cards_to_book=}'
     return None
