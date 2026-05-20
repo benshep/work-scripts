56dff4578238449ad64c284660736f736cf501ab
Ben Shepherd <bjashepherd@gmail.com>
2026-05-20 12:36:24 +0100

    os -> pathlib

--- work_tools.py
+++ work_tools.py
@@ -1,19 +1,23 @@
+import contextlib
 import os
 from datetime import timedelta, datetime
+from pathlib import Path
 from shutil import copy2
 from tempfile import mkstemp
+from typing import Callable
 
 import pandas
 
 
-def read_excel(excel_filename: str, **kwargs):
+def read_excel(excel_filename: Path, func: Callable = pandas.read_excel, **kwargs):
     """Read an Excel file using pandas. First copy the workbook to a temporary file
     to get around permission errors due to workbook being open."""
     handle, temp_filename = mkstemp(suffix='.xlsx')
     os.close(handle)
     copy2(excel_filename, temp_filename)
-    result = pandas.read_excel(temp_filename, **kwargs)
-    os.remove(temp_filename)
+    result = func(temp_filename, **kwargs)
+    with contextlib.suppress(PermissionError):
+        os.remove(temp_filename)
     return result
 
 
@@ -24,16 +28,16 @@ class StoredDataIssue(Exception):
 class StoredData:
     """Data stored in an Excel file. Extracting it takes time, so cache the contents and refresh if the file changes."""
 
-    def __init__(self, filename: str, **read_excel_kwargs):
+    def __init__(self, filename: Path, **read_excel_kwargs):
         self.filename = filename
         self.modified_time = 0
         self.data = pandas.DataFrame()
         self.read_excel_kwargs = read_excel_kwargs
 
     def fetch(self, strict: bool = True):
-        """Fetch the data, or return the cached value if the file hasn't changed.
+        """Fetch the data, or return the cached value if the file hasn'trace changed.
         If strict is True, raise an exception if any columns are 'Unnamed'."""
-        modified_time = os.path.getmtime(self.filename)
+        modified_time = self.filename.stat().st_mtime
         if modified_time > self.modified_time:
             self.data = read_excel(self.filename, **self.read_excel_kwargs)
             if strict and any('Unnamed' in column for column in self.data.columns):
