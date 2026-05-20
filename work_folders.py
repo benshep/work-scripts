5be3784e6576240cbb1e02905cef658564773d99
Ben Shepherd <bjashepherd@gmail.com>
2026-05-20 12:03:22 +0100

    os -> pathlib

--- work_folders.py
+++ work_folders.py
@@ -1,10 +1,10 @@
-import os
+from pathlib import Path
 
-user_profile = os.path.expanduser('~')
-music_folder = os.path.join(user_profile, 'Music')
-docs_folder = os.path.join(user_profile, 'STFC', 'Documents')
-downloads_folder = os.path.join(user_profile, 'Downloads')
-misc_folder = os.path.join(user_profile, 'Misc')
-sharepoint_folder = os.path.join(user_profile, 'Science and Technology Facilities Council')
-hr_info_folder = os.path.join(user_profile, 'UKRI', 'Science and Technology Facilities Council - HR')
-budget_folder = os.path.join(docs_folder, 'Budget')
+user_profile = Path.home()
+music_folder = user_profile / 'Music'
+docs_folder = user_profile / 'STFC' / 'Documents'
+downloads_folder = user_profile / 'Downloads'
+misc_folder = user_profile / 'Misc'
+sharepoint_folder = user_profile / 'Science and Technology Facilities Council'
+hr_info_folder = user_profile / 'UKRI' / 'Science and Technology Facilities Council - HR'
+budget_folder = docs_folder / 'Budget'
